ea71884b613af3cd15159c5f130219926adf50da
solomon <hansolomon168@gmail.com>
2023-04-04 20:38:23 +0600

    feat: update ws url

--- src/configs.ts
+++ src/configs.ts
@@ -1,4 +1,4 @@
-const WS_SERVER_URL = "wss://d80ktxonv3.execute-api.eu-central-1.amazonaws.com/dev";
+const WS_SERVER_URL = "wss://thenl548o7.execute-api.eu-central-1.amazonaws.com/dev";
 
 
 export {
